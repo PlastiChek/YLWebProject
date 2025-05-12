f2c65fbf5a5c84febd6cf56849f76acdb9c22b67
Admin <ulanapolancikova@gmail.com>
2025-05-12 23:32:01 +0300

    second

--- forms/AddingBasketForm.py
+++ forms/AddingBasketForm.py
@@ -7,4 +7,5 @@ class AddingBasketForm(FlaskForm):
     name = StringField('Название', validators=[DataRequired()])
     price = StringField('Цена', validators=[DataRequired()])
     provider = StringField('Поставщик', validators=[DataRequired()])
+    number = StringField('Кол-во', validators=[DataRequired()])
     submit = SubmitField('Добавить')
--- forms/DeleteForm.py
+++ forms/DeleteForm.py
@@ -7,4 +7,5 @@ class DeleteForm(FlaskForm):
     name = StringField('Название', validators=[DataRequired()])
     price = StringField('Цена', validators=[DataRequired()])
     provider = StringField('Поставщик', validators=[DataRequired()])
+    number = StringField('Кол-во', validators=[DataRequired()])
     submit = SubmitField('Удалить')
